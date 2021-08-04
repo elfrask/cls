768f97c2b5fd39a35adbfddbb2a91499a5dac01b
elfrask <elfraskodev@gmail.com>
2021-08-03 21:36:24 -0400

    CLSJS renew

---
+++ CLSJS/main.js
@@ -0,0 +1,21 @@
+let CLSAPP= require('./CLSJS');
+let fs= require('fs');
+let N = `
+`
+
+
+function main(g) {
+    let data = fs.readFileSync(g, "utf-8");
+    let app = CLSAPP.clsapp();
+    let d_crudo = app.desline(data, g)
+    let parseado = app.parselex(d_crudo);
+
+    fs.writeFileSync(g +".json", JSON.stringify(parseado, N, 4))
+};
+
+
+
+
+main(process.argv[2])
+
+
